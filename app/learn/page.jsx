a99266f77d785759792bef7deac60256ec1698a8
ash_tasnim_1834 <tasnim.iterator.34@gmail.com>
2024-11-08 10:43:47 +0600

    Seacrh text box and button added

--- app/learn/page.jsx
+++ app/learn/page.jsx
@@ -1,10 +1,28 @@
 "use client"
-
+import React, { useState, useEffect } from "react";
 
 const LearnPage=()=>{
+    const [searchTerm, setSearchTerm] = useState("");
+
+
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        router.push(`/search?query=${searchTerm}`);
+      };
+    
     return(
         <div>
-            <h1>learn</h1>
+           <header>
+            <div className="search-bar"></div>
+            <input
+             type="text"
+             place="Search for coding videos ..."
+             value={searchTerm}
+             onChange={(e) => setSearchTerm(e.target.value)}
+            />
+                 <button onClick={handleSearch}>Search</button>
+           </header>
         </div>
     );
 }
